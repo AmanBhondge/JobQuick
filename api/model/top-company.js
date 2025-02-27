6498cfa098063c2fa0f859392bdaf24277038192
Aman Bhondge <amanbhondge@gmail.com>
2025-02-27 12:22:38 +0530

    70

--- api/model/top-company.js
+++ api/model/top-company.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const topCompanySchema = new mongoose.Schema({
     companyName : { type: String, required: true },
-    compantyEmail : { type: String, required: true },
+    companyEmail : { type: String, required: true },
     companyURL : { type: String, required: true },
     companyDescription : { type: String, required: true }
     });
